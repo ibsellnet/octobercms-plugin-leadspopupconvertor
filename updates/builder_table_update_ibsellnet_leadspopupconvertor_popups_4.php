eb1f4267a70319f12f70cd6f5ece78750505c97e
Manea Raluca (Ibsell.NET) <developer@ibsell.net>
2019-10-31 21:09:10 +0200

    - fix issue on installation: see reference here: https://octobercms.com/plugin/support/ibsellnet-leadspopupconvertor/error-installing-too-long-content-text-in-seeder

---
+++ updates/builder_table_update_ibsellnet_leadspopupconvertor_popups_4.php
@@ -0,0 +1,23 @@
+<?php namespace Ibsellnet\Leadspopupconvertor\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateIbsellnetLeadspopupconvertorPopups4 extends Migration
+{
+    public function up()
+    {
+        Schema::table('ibsellnet_leadspopupconvertor_popups', function($table)
+        {
+            $table->text('content')->nullable(false)->unsigned(false)->default(null)->change();
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('ibsellnet_leadspopupconvertor_popups', function($table)
+        {
+            $table->string('content', 191)->nullable(false)->unsigned(false)->default(null)->change();
+        });
+    }
+}
