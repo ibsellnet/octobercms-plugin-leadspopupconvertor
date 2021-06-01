ab705bb7c12c0b4756ab44aec259b7d89cccde41
Manea Raluca (Ibsell.NET) <developer@ibsell.net>
2021-06-01 09:41:41 +0300

    Update plugin as per OctoberCMS requirements: https://octobercms.com/docs/help/publishing-packages

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,6 @@
 <?php return [
     'plugin' => [
         'name' => 'LeadsPopupConvertor',
-        'description' => 'Plugin desc'
+        'description' => 'An OpenSource plugin for OctoberCMS that helps you generate popups depending on more user actions.'
     ]
 ];
